c95bc5d6e3433db451792dafe06398d4f441460f
João Paulo Pacheco <joaopaulo.pacheco@outlook.com.br>
2020-09-01 16:29:45 -0300

    Continuando implemtacao dos movimentos

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -13,16 +13,7 @@ namespace Xadrez
                 Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
-                    if (tab.peca(i, j) == null)
-                    {
-                        Console.Write("- ");
-                    }
-                    else
-                    {
-                        imprimirPeca(tab.peca(i, j));
-                        Console.Write(" ");
-
-                    }
+                    imprimirPeca(tab.peca(i, j));
 
                 }
                 Console.WriteLine();
@@ -35,21 +26,29 @@ namespace Xadrez
             string s = Console.ReadLine();
             char coluna = s[0];
             int linha = int.Parse(s[1] + "");
-            return new PosicaoXadrez(coluna,linha);
+            return new PosicaoXadrez(coluna, linha);
         }
 
         public static void imprimirPeca(Peca peca)
         {
-            if (peca.cor == Cor.Branca)
+            if (peca == null)
             {
-                Console.Write(peca);
+                Console.Write(" -");
             }
             else
             {
-                ConsoleColor aux = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write(peca);
-                Console.ForegroundColor = aux;
+                if (peca.cor == Cor.Branca)
+                {
+                    Console.Write(peca);
+                }
+                else
+                {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write(peca);
+                    Console.ForegroundColor = aux;
+                }
+
             }
         }
 
